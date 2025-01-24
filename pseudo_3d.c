0d8137802f4d48fc28cbf9b0c7bcd62cc3b4597f
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-24 17:02:32 +0000

    fix movement

--- pseudo_3d.c
+++ pseudo_3d.c
@@ -3,35 +3,39 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
-float move_f( const short int map_arr[], float location[], float direction, float rot, short int mod, bool noclip, const float pi ) {
+void move_f( const short int map_arr[], float *location, float direction, float rot, float mod, bool noclip, const float pi ) {
 	float tmp[] = { location[0], location[1] };
-	tmp[0] += 0.125 * sin(direction * pi)*mod;
-	tmp[1] += 0.125 * cos(direction * pi)*mod;
+//    printf("all %fx %fy\n", tmp[0], tmp[1]);
+	tmp[0] += 0.125 * sin(direction * pi) * mod;
+// 	printf("%f\n", 0.125 * sin(direction * pi) * mod);
+// 	printf("%f %f %f\n", sin(direction * pi), direction, pi);
+	tmp[1] += 0.125 * cos(direction * pi) * mod;
+//	printf("%f\n", 0.125 * cos(direction * pi) * mod);
+//    printf("all %fx %fy\n", tmp[0], tmp[1]);
     int index[] = {ceil(tmp[0]), ceil(tmp[1])};
-	if(index[0]<9 && index[0]>0 && index[1]<12 && map_arr[index[0]] != 1) {
-        return * tmp;
-    }
-    else {
-        return * location;
+	if(index[0]<9 && index[0]>0 && index[1]<12 && map_arr[index[0]*10+index[1]] != 1) {
+        printf("tmp %fx %fy\n", tmp[0], tmp[1]);
+        location[0] = tmp[0];
+        location[1] = tmp[1];
     }
 	printf("move\n");
 }
 
-float rad_ch(float direction, float rot) {
-	direction += rot;
-	if(direction>=2) {
-		direction -=  2;
+void rad_ch(float *direction, float rot) {
+	*direction += rot;
+	if(*direction>=2) {
+		*direction -=  2;
 	}
-	else { if(direction<0) {
-		direction += 2;
+	else { if(*direction<0) {
+		*direction += 2;
 	}
 	}
-	return direction;
 }
 
 void raycast(float direction, const short int map_arr[], const float step, const float shift) {
 	float hit[200] = {};
-	float angle = rad_ch(direction, shift);
+    float angle = direction;
+	rad_ch(&angle, shift);
 	int i = 0;
 	for (i = 0; i < 200; i+=2) {
 		printf("i");
@@ -40,7 +44,7 @@ void raycast(float direction, const short int map_arr[], const float step, const
 			printf("add");
 			mov += 0.1;
 		}
-		angle = rad_ch(direction, step);
+		rad_ch(&angle, step);
 	}
 	printf("raycast");
 }
@@ -71,16 +75,30 @@ void visual(float direction, const short int map_arr, float location, const shor
 
 int main(void) {
 	const float pi = 3.141592;
-	const short int map_arr[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+	const short int map_arr[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                  1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 	const short int length = 100;
 	const short int h = 500;
 	const short int sacle = 5;
-	float loaction[2] = {1, 1};
+	float location[2] = {2, 2};
 	float direction = 0;
 	bool show_map = false;
 	bool noclip = false;
-	float mod = 0.5;
+	float mod = 1;
 	short int move_tic = 1;
-	printf("main\n");
+	printf("main\n%fx %fy\n", location[0], location[1]);
+    move_f(map_arr, &*location, direction, 0.5, mod, false, pi);
+	printf("%fx %fy\n", location[0], location[1]);
     return 0;
 }
