d05c4fe2c341413efb89d31a5e1778ba0516d205
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-28 18:35:42 +0000

    make hit and line arrays independant

--- pseudo_3d.c
+++ pseudo_3d.c
@@ -32,11 +32,12 @@ void rad_ch(float *direction, float rot) {
 //    printf("%f\n", *direction);
 }
 
-float* raycast(float direction, const short int map_arr[], float *location, const float step, const float shift, const float pi) {
-    float *hit = (float *)malloc(200*sizeof(float));
+float* raycast(float direction, const short int map_arr[], float *location, const float step, const float shift, const float pi, int *hit_size) {
+    float hit[200] = {};
     memset(hit, -1, 200*sizeof(*hit));
     float angle = direction;
 	rad_ch(&angle, shift);
+    short int rays_num = 0;
 	int i = 0;
 	int j = 0;
     int index[2] = {};
@@ -49,9 +50,9 @@ float* raycast(float direction, const short int map_arr[], float *location, cons
             if (index[0] < 1 || index[0] > 9 || index[1] < 1 || index[1] > 12) {
                 hit[i] = angle;
                 hit[i+1] = mov;
+                rays_num += 2;
                 break;
             }
-            //map_arr[ceil(location[0] + mov * sin(angle * pi))][ceil(location[1] + mov * cos(angle * pi))] == "#"
             //printf("index %dx %dy\n", index[0], index[1]);
 //            if(map_arr[index[0]+index[1]*10] == 1) {
 //                hit[i] = angle;
@@ -64,51 +65,47 @@ float* raycast(float direction, const short int map_arr[], float *location, cons
 //        printf("%f %f\n", hit[i], hit[i+1]);
 //        if (hit[i] != hit[i]) { will check if nan
 	}
-    return hit;
+    float *out_hit = (float *)malloc(rays_num*sizeof(float));
+    short int out_counter = 0;
+    for (i=0; i<200; i+=2) {
+        if(hit[i]<0) { continue; }
+        out_hit[out_counter] = hit[i];
+        out_hit[out_counter+1] = hit[i+1];
+//        printf("out %f %f\n", hit[i], hit[i+1]);
+        out_counter += 2;
+    }
+//    printf("%d\n", out_counter);
+    *hit_size = out_counter;
+    return out_hit;
 }
 
-//void print_map( const short int map_arr[], float direction, float location[], float hit[]) {
-//	printf("map");
-//}
-
-float* visual(float direction, const short int map_arr, float *location, const short int h, bool show_map, bool noclip, const float step, const float shift, const float pi) {
+int* visual(float direction, const short int map_arr, float *location, const short int h, bool show_map, bool noclip, const float step, const float shift, const float pi, float *hit, int hit_size) {
 	int start = 0;
 	int end = 0;
 //	int lines[200] = {};
-    float *lines = (float *)malloc(400*sizeof(float));
-    memset(lines, -1, 400*sizeof(*lines));
+    int *lines = (int *)malloc(hit_size*sizeof(int));
     float dist = 0;
     int i = 0;
-    float* hit = raycast(direction, &map_arr, location, 0.005, -0.25, pi);
-    int index = 0;
     //printf("%f\n", hit);
-    for (i = 0; i < 200; i+=2) {
-        if(hit[i]!=hit[i]) { continue; }
-        else {
+    for (i = 0; i < hit_size; i+=2) {
+        start = 0;
+        end = h - 1;
+        dist = hit[i+1];
+        if(dist != 0) {
+            start = h / 2 * (1 - 1/dist);
+            end = h / 2 * (1 + 1/dist);
+        }
+        if(start<0) {
             start = 0;
+        }
+        if(end>h) {
             end = h - 1;
-            dist = hit[i+1];
-            if(dist != 0) {
-                start = h / 2 * (1 - 1/dist);
-                end = h / 2 * (1 + 1/dist);
-            }
-            if(start<0) {
-                start = 0;
-            }
-            if(end>h) {
-                end = h - 1;
-            }
-            lines[i] = start;
-            lines[i+1] = end;
+        }
+        lines[i] = start;
+        lines[i+1] = end;
 //            printf("%d %d\n", lines[i], lines[i+1]);
 //            printf("%d %d\n", start, end);
-        }
     }
-    for (i=200; i<400; i++) {
-        lines[i] = hit[i-200];
-    }
-    free(hit);
-    hit = NULL;
     return lines;
 }
 
@@ -136,6 +133,7 @@ int main(void) {
 	bool noclip = false;
 	float mod = 1;
 	short int move_tic = 1;
+    int hit_size = 0;
 	printf("main\n%fx %fy\n", location[0], location[1]);
 //    move_f(map_arr, &*location, direction, 0.5, mod, false, pi);
 	printf("%fx %fy\n", location[0], location[1]);
@@ -178,14 +176,15 @@ int main(void) {
         SDL_RenderClear(renderer);
  
         SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
-        float* line = visual(direction, *map_arr, &*location, h, show_map, noclip, 0.005, -0.25, pi);
+        float* hit = raycast(direction, *&map_arr, location, 0.005, -0.25, pi, &hit_size);
+//        printf("hit size %d\n", hit_size);
+        int* line = visual(direction, *map_arr, &*location, h, show_map, noclip, 0.005, -0.25, pi, hit, hit_size);
         int i = 0;
         int j = 0;
-        for (i=0; i<200; i+=2) {
-            if (round(line[i]) < 0 || round(line[i+0]) < 0) { continue; }
+        for (i=0; i<hit_size; i+=2) {
 //            printf("drawing a line\n%d %d\n", line[i], line[i+1]);
             for (j=0; j<=scale*2; j++) {
-                SDL_RenderDrawLine(renderer, i*scale + j, round(line[i]), i * scale + j, round(line[i+1]));
+                SDL_RenderDrawLine(renderer, i*scale + j, line[i], i * scale + j, line[i+1]);
             }
         }
         SDL_SetRenderDrawColor( renderer, 0, 0, 242, 255 );
@@ -208,15 +207,15 @@ int main(void) {
         r.w = 3;
         r.h = 3;
         SDL_RenderDrawRect( renderer, &r );
-        for (i=200; i<400; i+=2) {
-            if(round(line[i]) >= 0) {
-                SDL_RenderDrawLine(renderer, round(location[0]*10 + 5), round(location[1]*10 + 5), ceil((location[0] + line[i+1] * cos(line[i] * pi)) * 10), ceil((location[1] + line[i+1] * sin(line[i] * pi)) * 10));
+        for (i=0; i<hit_size; i+=2) {
+            SDL_RenderDrawLine(renderer, round(location[0]*10 + 5), round(location[1]*10 + 5), ceil((location[0] + line[i+1] * cos(line[i] * pi)) * 10), ceil((location[1] + line[i+1] * sin(line[i] * pi)) * 10));
 //                printf("%f %f\n", line[i], line[i + 1]);
-            }
         }
 //        printf("\n");
         free(line);
+        free(hit);
         line = NULL;
+        hit = NULL;
         // TODO rendering code goes here
  
         // render window
