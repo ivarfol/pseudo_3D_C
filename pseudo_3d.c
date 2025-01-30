6f7d88a68ed6f9bd4eaa874bf44536b8e0f4c7f6
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-30 10:32:49 +0000

    remove out_hit

--- pseudo_3d.c
+++ pseudo_3d.c
@@ -35,15 +35,17 @@ void rad_ch(float *direction, float rot) {
 }
 
 float* raycast(float direction, const short int map_arr[][10], float *location, const float step, const float shift, int *hit_size) {
-    float hit[200] = {};
-//    memset(hit, -1, 200*sizeof(*hit));
+    float *hit = (float *)malloc(200*sizeof(float));
+    if(hit==NULL) {
+        printf("Failed to allocate memory!\n");
+        exit(1);
+    }
     float angle = direction;
 	rad_ch(&angle, shift);
     short int rays_num = 0;
 	int i;
 	int j;
     int index[2] = {};
-//    float mov;
     float len_y;
     float len_x;
     float delta_x;
@@ -58,7 +60,6 @@ float* raycast(float direction, const short int map_arr[][10], float *location, 
 	wall_hit = 0;
         quad_x = floor(location[0]);
         quad_y = floor(location[1]);
-//		mov = 0; // change for const step per ray
 //        printf("%fx %fy  quad_x %d quad_y %d ", location[0], location[1], quad_x, quad_y);
         delta_x = abs(1 / cos(angle * pi));
         delta_y = abs(1 / sin(angle * pi));
@@ -82,13 +83,11 @@ float* raycast(float direction, const short int map_arr[][10], float *location, 
         while (wall_hit == 0) {
             if (len_x < len_y ) {
                 len_x += delta_x;
- //               if (location[1] + len_x * sin(angle * pi) > 13) {continue;}
                 quad_x += stepX;
                 side = 0;
             }
             else {
                 len_y += delta_y;
-//                if (location[0] + len_y * cos(angle * pi) > 10) {continue;}
                 quad_y += stepY;
                 side = 1;
             }
@@ -107,43 +106,12 @@ float* raycast(float direction, const short int map_arr[][10], float *location, 
 //		printf(" y  %fx %fy  length %f  angle %f\n", location[0] + len_y * cos(angle * pi), location[1] + len_y * sin(angle * pi), len_y, angle);
 	}
         rays_num += 2;
-//		for (j = 0; j < 1000; j++) {
-//            index[0] = ceil(location[0] + mov * cos(angle * pi));
-//            index[1] = ceil(location[1] + mov * sin(angle * pi));
-//            if (index[0] < 1 || index[0] > 8 || index[1] < 1 || index[1] > 11) {
-//                hit[i] = angle;
-//                hit[i+1] = mov;
-//                rays_num += 2;
-//                break;
-//            }
-            //printf("index %dx %dy\n", index[0], index[1]);
-//            if(map_arr[index[0]+index[1]*10] == 1) {
-//                hit[i] = angle;
-//                hit[i+1] = mov;
-//                break;
-//            }
-//			mov += 0.1;
-//		}
-		rad_ch(&angle, step);
-//        printf("%f %f\n", hit[i], hit[i+1]);
-//        if (hit[i] != hit[i]) { will check if nan
-	}
-    float *out_hit = (float *)malloc(rays_num*sizeof(float));
-    if(out_hit==NULL) {
-        printf("Failed to allocate memory!\n");
-        exit(1);
-    }
-    short int out_counter = 0;
-    for (i=0; i<200; i+=2) {
-        if(hit[i]<0) { continue; }
-        out_hit[out_counter] = hit[i];
-        out_hit[out_counter+1] = hit[i+1];
+	rad_ch(&angle, step);
 //        printf("out %f %f\n", hit[i], hit[i+1]);
-        out_counter += 2;
     }
 //    printf("%d\n", out_counter);
-    *hit_size = out_counter;
-    return out_hit;
+    *hit_size = rays_num;
+    return hit;
 }
 
 int* visual(float direction, float *location, const short int h, bool show_map, bool noclip, const float step, const float shift, float *hit, int hit_size) {
