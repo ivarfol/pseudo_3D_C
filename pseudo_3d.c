73784287eb87f9f95f755c1da684dfb858e97e26
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-30 21:49:54 +0000

    use radians

--- pseudo_3d.c
+++ pseudo_3d.c
@@ -8,19 +8,19 @@
 
 void rad_ch(float *direction, float rot) {
 	*direction += rot;
-	if(*direction>=2) {
-		*direction -=  2;
+	if(*direction>=2 * PI) {
+		*direction -=  2 * PI;
 	}
 	else { if(*direction<0) {
-		*direction += 2;
+		*direction += 2 * PI;
 	}}
 }
 
 void move_f( const short int map_arr[][10], float *location, float direction, float rot, float mod, bool noclip) {
 	float angle = direction;
 	rad_ch(&angle, rot);
-	float x = location[0] + 0.125 * cos(angle * PI) * mod;
-	float y = location [1] + 0.125 * sin(angle * PI) * mod;
+	float x = location[0] + 0.125 * cos(angle) * mod;
+	float y = location [1] + 0.125 * sin(angle) * mod;
     int round_x = round(x);
     int round_y = round(y);
     if (map_arr[round_x][round_y] == 0) {
@@ -47,7 +47,7 @@ int main(void) {
 	const short int h = 500;
 	const short int scale = 5;
 	float location[2] = {2, 2};
-	float direction = 1.5;
+	float direction = 1.5 * PI;
 	bool show_map = false;
 	bool noclip = false;
 	float mod = 1;
@@ -82,16 +82,16 @@ int main(void) {
             // TODO input handling code goes here
 //        }
  
-        rad_ch(&direction, -0.01);
-        move_f(map_arr, &*location, direction, 0.5, mod, false);
+        rad_ch(&direction, -0.01 * PI);
+        move_f(map_arr, &*location, direction, 0.5 * PI, mod, false);
         // clear window
  
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
  
         // get hits
-        float shift = -0.25;
-        float step = 0.005;
+        float shift = -0.25 * PI;
+        float step = 0.005 * PI;
 	    float angle = direction;
 		rad_ch(&angle, shift);
 	    short int rays_num = 0;
@@ -118,8 +118,8 @@ int main(void) {
 		for (k = 0; k < 200; k+=2) {
             wall_hit = 0;
             len_x = 0;
-            sin_a = sin(angle * PI);
-            cos_a = cos(angle * PI);
+            sin_a = sin(angle);
+            cos_a = cos(angle);
             //horizontals
             if (sin_a > 0) {
                 y_hor = map_pos[1] + 1;
@@ -228,7 +228,7 @@ int main(void) {
         r.h = 3;
         SDL_RenderDrawRect( renderer, &r );
         for (i=0; i<hit_size; i+=2) {
-            SDL_RenderDrawLine(renderer, round(location[0]*10 + 5), round(location[1]*10 + 5), ceil((location[0] + hit[i+1] * cos(hit[i] * PI)) * 10), ceil((location[1] + hit[i+1] * sin(hit[i] * PI)) * 10));
+            SDL_RenderDrawLine(renderer, round(location[0]*10 + 5), round(location[1]*10 + 5), ceil((location[0] + hit[i+1] * cos(hit[i])) * 10), ceil((location[1] + hit[i+1] * sin(hit[i])) * 10));
         }
         // render window
  
